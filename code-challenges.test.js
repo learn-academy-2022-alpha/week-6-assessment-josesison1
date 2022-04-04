00cef62f92222bbc2ef3732677ad30d78e5de02c
josesison1 <learnacademy@LEARNs-Air.lan1>
2022-04-04 09:04:55 -0700

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,38 +14,129 @@
 
 // a) Create a test with an expect statement using the variable provided.
 
-const people = [
+// Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+
+describe("capitalize", () => {
+  it("takes in an array of objects and returns an array with a sentence about each person with their name capitalized.", () => {
+    const people = [
   { name: "ford prefect", occupation: "a hitchhiker" },
   { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
-  { name: "arthur dent", occupation: "a radio employee" }
-]
-// Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+  { name: "arthur dent", occupation: "a radio employee" }]
+    expect(capitalize(people)).toEqual(["Ford Prefect is a hitchhiker", "Zaphod Beeblebrox is president of the galaxy", "Arthur Dent is a radio employee"])
+  })
+})
+
+
+//======================== GOOD ERROR FAILURE ===============================//
+// ● sumCubed › takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
+
+// ReferenceError: sumCubed is not defined
+//===========================================================================//
 
 
 // b) Create the function that makes the test pass.
+//========================== PSEUDO CODE ====================================//
+//create a function named 'capitalize'
+//must take in an array of object as a parameter, we'll call it 'arr'
+//declare a new variable called 'newArr' and set it to arr.map to iterate through the values of the array
+//declare a new variable called 'name' and set it to the values.name.split.map which would refer to the 'name' key of the object and will split the name by spaces into 2 seperate indexes
+//iterate through the array of the 'name' key and refer to the first letter of each index by using .charAt() method and refer to its 0 index
+//capitalize it by using .toUpperCase() method, and add the value back into the array by using .substring(1) which would add the rest of the word without the first letter included
+//return the variable 'name' which contains the capitalized letters and use .join() method to join the first name and last name into 1 index
+//return the name plus the word "is" and refer to the key value of occupation to print out that value, which would be the whole completed sentence. 
+//===========================================================================//
+
+
+const capitalize = (arr) => {
+    let newArr = arr.map(v => {
+      let name = v.name.split(" ").map(val => val.charAt(0).toUpperCase() + val.substring(1) 
+      )
+      return v = name.join(" ") + ' is ' +  v.occupation 
+    })
+    return newArr
+}
+
+
+//TEST PASSED! 
+
 
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
-// Expected output: [ 2, 0, -1, 0 ]
-const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
-// Expected output: [ 2, 1, -1 ]
+describe("remainder", () => {
+  it("takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.", () => {
+    const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
+    // Expected output: [ 2, 0, -1, 0 ]
+    const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
+    // Expected output: [ 2, 1, -1 ]
+    expect(remainder(hodgepodge1)).toEqual([ 2, 0, -1, 0 ])
+    expect(remainder(hodgepodge2)).toEqual([ 2, 1, -1 ])
+  })
+})
+//======================== GOOD ERROR FAILURE ===============================//
+// ● remainder › takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
+
+// ReferenceError: remainder is not defined
+//===========================================================================//
 
 
 // b) Create the function that makes the test pass.
+//========================== PSEUDO CODE ====================================//
+//create a function called 'remainder'.
+//must take in an array of different data types as a parameter and will name the parameter 'arr'.
+//declare a new variable called 'newArr' and set it to arr.filter, and filter out the non-numbers by using typeof method and set it strictly equals to equal 'number' which is addressing to the data type.
+//once that is filtered, return the 'newArr' and use .map to iterate through the values inside the array and return the values modulo 3 which would return you the remainder of each value after it is divided by 3.
+//===========================================================================//
+
+const remainder = (arr) => {
+  let newArr = arr.filter(v => typeof v === 'number') 
+    return newArr.map(v => v % 3)
+}
+
+
+//TEST PASSED! 
+
 
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const cubeAndSum1 = [2, 3, 4]
-// Expected output: 99
-const cubeAndSum2 = [0, 5, 10]
-// Expected output: 1125
+describe("sumCubed", () => {
+  it("takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.", () => {
+    const cubeAndSum1 = [2, 3, 4]
+    // Expected output: 99
+    const cubeAndSum2 = [0, 5, 10]
+    // Expected output: 1125
+    expect(sumCubed(cubeAndSum1)).toEqual(99)
+    expect(sumCubed(cubeAndSum2)).toEqual(1125)
+  })
+})
+
+//======================== GOOD ERROR FAILURE ===============================//
+// ● sumCubed › takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
+
+// ReferenceError: sumCubed is not defined
+//===========================================================================//
 
 
 // b) Create the function that makes the test pass.
+//========================== PSEUDO CODE ====================================//
+//create a function named 'sumCubed'
+//must take in an array of numbers as a parameter, we'll name it 'arr'
+//declare a variable named 'sum' and set it equals to 0 
+//iterate through the values of the array by using a .filter() 
+//return the sum plus the value of the array after being cubed
+//return the variable sum which contains all the values added together after being cubed
+//===========================================================================//
+
+
+const sumCubed = (arr) => {
+  let sum = 0 
+  arr.filter(v => sum += v ** 3
+  )
+  return sum
+}
+
--- rails-commenting.rb
+++ rails-commenting.rb
@@ -6,25 +6,25 @@
 
 # FILE: app/controller/blog_posts_controller.rb
 
-# ---1)
+# ---1) This is a a controller called "BlogPosts" and it is inheriting from the "Application" controller.
 class BlogPostsController < ApplicationController
   def index
-    # ---2)
+    # ---2) An instance variable called "posts" is declared and is set to all the values in a table called "BlogPost".
     @posts = BlogPost.all
   end
 
-  # ---3)
+  # ---3) A "show" method is defined and an instance variable called "post" contains all values in the "BlogPost" table. Each value in that table can be accessed by specificly calling onto its id/primary key. 
   def show
     @post = BlogPost.find(params[:id])
   end
 
-  # ---4)
+  # ---4) A method named "new" is defined and an instance variable called "post" is set to BlogPost.new which will let you input and create new data into the table. 
   def new
     @post = BlogPost.new
   end
 
   def create
-    # ---5)
+    # ---5) in the create method, an instance variable named "post" is set to BlogPost.create that contains a strong params named "blog_post_params". This method lets the user create new blog posts, if the post is valid, it will redirect to the page where you could view all the existing posts along with the new one that was just recently created. Else it will remain on that page where you are able to create a new post. 
     @post = BlogPost.create(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -34,13 +34,13 @@ class BlogPostsController < ApplicationController
   end
 
   def edit
-    # ---6)
+    # ---6) In the edit method, an instance variable named "post" is defined and set to BlogPost.find that takes a parameter of an id/primary key. This method lets you access an existing blog post by addressing it by its id or primary key and you will be able to edit and make changes to it.
     @post = BlogPost.find(params[:id])
   end
 
   def update
     @post = BlogPost.find(params[:id])
-    # ---7)
+    # ---7) in this update method, an instance variable called 'post' is set to update method that takes in a strong params of 'blog_post_params'. the method contains a conditional statement, if the post is valid which is referring to if there are no errors or if they meet the required validations then it will redirect it to the index page that contains all blog post. If it does not meet the validation then it will go to the page where you can edit the blog post.
     @post.update(blog_post_params)
     if @post.valid?
       redirect_to blog_post_path(@post)
@@ -54,15 +54,15 @@ class BlogPostsController < ApplicationController
     if @post.destroy
       redirect_to blog_posts_path
     else
-      # ---8)
+      # ---8) in this destroy method, the line below will stay on the same page/view if this else condition is met
       redirect_to blog_post_path(@post)
     end
   end
 
-  # ---9)
+  # ---9) The private keyword is declared which means this method cannot be called outside of this class. Also anything below the keyword 'private' is protected which is the reason why params are usually the last methods in a controller.
   private
   def blog_post_params
-    # ---10)
+    # ---10) Strong params have two methods, which are 'require' and 'permit'. it requires the attributes passed in, in order to create or update methods and permit allows certain values to be present. 
     params.require(:blog_post).permit(:title, :content)
   end
 end
